560acb905dd11f6b91c102479cf51412eb361089
Hentryx <lord_ergoth@hotmail.com>
2017-08-02 11:12:53 -0500

    añadido Json ejemplo
    se añade el codigo para leer y hacer parse del objeto dsl para
    modelado

--- server.js
+++ server.js
@@ -6,6 +6,21 @@ var express = require('express'),
     router,
     server;
 app = express();
+var dsl = require('./ejemplo.json');
+
+var name=dsl.name;
+var condition=dsl.condition;
+var condtype=condition.type;
+var parta=condition.inputs[0][0];
+var partb=condition.inputs[0][1];
+var subconditiona=parta.condition;
+var subelementaa=parta.a[0][0].field;
+var subelementab=parta.b[0][0].value;
+var subconditionb=partb.condition;
+var subelementba=partb.a[0][0].field;
+var subelementbb=partb.b[0][0].field;
+
+
 mongoose.connect('mongodb://localhost/prueba1', function(err, res){
     var msg="Connected";
     if(err) msg="Error connecting: "+err.message;
@@ -14,20 +29,7 @@ mongoose.connect('mongodb://localhost/prueba1', function(err, res){
 app.use(bodyparser.json());
 router=express.Router();
 router.get('/',function(req, res){
-    res.json([
-        {
-            id: "1",
-            name:"Viviana"
-        },
-        {
-            id: "2",
-            name:"Isabella"
-        },
-        {
-            id: "3",
-            name:"Henry"
-        },
-    ]);
+    res.json({test:name+"("+subelementaa+subconditiona+subelementab+" "+condtype+" "+subelementba+subconditionb+subelementbb+")"});
 });
 app.use('/familia',router);
 server = http.createServer(app);
